fe8a4995b21f65fe7b5be3d67ddf77e5055641c6
Tor Arne Torgersen <6398417+torarnet@users.noreply.github.com>
2025-08-13 12:35:07 +0200

    Ts 96 fix host url error causing failure (#120)
    * ts-98: fix failing upstream fetch because of url using host to specify path causing escaping of path delimiters
    
    * ts-98: change to runtimeonly to satisfy nebula linter
    
    * ts-98: logging auth id
    
    * ts-98: small fixes

--- src/main/java/no/unit/authority/AuthoritySruConnection.java
+++ src/main/java/no/unit/authority/AuthoritySruConnection.java
@@ -5,9 +5,15 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.apache.hc.core5.net.URIBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AuthoritySruConnection {
+
+    private static final Logger logger = LoggerFactory.getLogger(AuthoritySruConnection.class);
 
     public static final String HTTPS = "https";
     public static final String VERSION_KEY = "version";
@@ -16,8 +22,11 @@ public class AuthoritySruConnection {
     public static final String OPERATION_SEARCH_RETRIEVE = "searchRetrieve";
     public static final String QUERY_KEY = "query";
     public static final String QUERY_FIELD = "rec.identifier";
+    public static final String CONNECTING_TO = "Connecting to {}";
+    public static final String PATH_DELIMITER = "/";
 
     public InputStreamReader connect(URL url) throws IOException {
+        logger.info(CONNECTING_TO, url.toString());
         return new InputStreamReader(url.openStream());
     }
 
@@ -32,11 +41,24 @@ public class AuthoritySruConnection {
     public URL generateQueryUrl(String authorityId) throws URISyntaxException, MalformedURLException {
         return new URIBuilder()
                 .setScheme(HTTPS)
-                .setHost(Config.getInstance().getAuthoritySruHost())
-                .setPath("/")
+                .setHost(getAuthoritySruHost())
+                .setPath(getAuthoritySruPath())
                 .setParameter(VERSION_KEY, SRU_VERSION_1_2)
                 .setParameter(OPERATION_KEY, OPERATION_SEARCH_RETRIEVE)
                 .setParameter(QUERY_KEY, String.format("%s=%s", QUERY_FIELD, authorityId))
                 .build().toURL();
     }
+
+    private String getAuthoritySruHost() {
+        return Stream.of(Config.getInstance().getAuthoritySruHost().split(PATH_DELIMITER))
+                   .findFirst()
+                   .orElse(Config.getInstance().getAuthoritySruHost());
+    }
+
+    private String getAuthoritySruPath() {
+        return Stream.of(Config.getInstance().getAuthoritySruHost().split(PATH_DELIMITER))
+                   .skip(1)
+                   .collect(Collectors.joining(PATH_DELIMITER));
+    }
+
 }
--- src/main/java/no/unit/authority/Config.java
+++ src/main/java/no/unit/authority/Config.java
@@ -1,6 +1,11 @@
 package no.unit.authority;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public final class Config {
+
+    private static final Logger logger = LoggerFactory.getLogger(Config.class);
 
     public static final String MISSING_ENVIRONMENT_VARIABLES = "Missing environment variables SRU_ENDPOINT";
     public static final String CORS_ALLOW_ORIGIN_HEADER_ENVIRONMENT_NAME = "ALLOWED_ORIGIN";
@@ -16,7 +21,13 @@ public final class Config {
 
         private static final Config INSTANCE = new Config();
 
+        public static final String INITIALIZING_CONFIG =
+            "Initializing Config... SRU_ENDPOINT: {} and ALLOWED_ORIGIN: {}";
+
         static {
+            logger.info(INITIALIZING_CONFIG,
+                        System.getenv(SRU_ENDPOINT_KEY),
+                        System.getenv(CORS_ALLOW_ORIGIN_HEADER_ENVIRONMENT_NAME));
             INSTANCE.setAuthoritySruHost(System.getenv(SRU_ENDPOINT_KEY));
             INSTANCE.setCorsHeader(System.getenv(CORS_ALLOW_ORIGIN_HEADER_ENVIRONMENT_NAME));
         }
